dfedace609eea441624b0de4510498dabb27eb45
Jacksun84 <bruno.sol.js@gmail.com>
2025-10-08 00:04:59 +0100

    adding logs

--- src/android/Zoom.java
+++ src/android/Zoom.java
@@ -318,6 +318,7 @@ public class Zoom extends CordovaPlugin implements ZoomSDKInitializeListener, Me
                     ", internalErrorCode="+internalErrorCode);
             pluginResult.setKeepCallback(true);
             this.callStatusCallback.sendPluginResult(pluginResult);
+        }
 
         sendMeetingCallback(meetingStatus);
     }
