7b639d3c4375ad96ff7a983b57cb0fa421e7e2b1
Jacksun84 <bruno.sol.js@gmail.com>
2025-10-14 02:34:32 +0100

    add after_compile hook file

--- hooks/manifest_update_delay.js
+++ hooks/manifest_update_delay.js
@@ -1,5 +1,12 @@
 module.exports = function(context) {
     const exec = require('child_process').exec;
+    const path = require('path');
+
+    // Get the project root
+    const projectRoot = context.opts.projectRoot;
+
+    // Build the correct path to the script (adjust the path as necessary)
+    const scriptPath = path.join(projectRoot, 'plugins/cordova.plugin.zoom/hooks/zzzz_add_networkSecurityConfig_manifest.js');
 
     // Function to be executed with a delay
     function delayedExecution() {
@@ -16,5 +23,14 @@ module.exports = function(context) {
     }
 
     // Delay the execution by 5 seconds (5000 ms)
+    console.log("Waiting for 5 seconds before executing delayed task...");
     setTimeout(delayedExecution, 5000); // Change 5000 to the delay you need in ms
+
+    /*
+    return Q.delay(5000)  // Delay for 5 seconds using Q library
+        .then(delayedExecution)  // Execute the delayed function after the delay
+        .catch((err) => {
+            console.error("Error during delayed execution:", err);
+        });
+    */
 };
