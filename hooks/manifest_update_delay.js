6e997c0f4ec1c7307f254a3c846b48a6373255e1
Jacksun84 <bruno.sol.js@gmail.com>
2025-10-14 01:54:30 +0100

    add after_compile hook file

---
+++ hooks/manifest_update_delay.js
@@ -0,0 +1,20 @@
+module.exports = function(context) {
+    const exec = require('child_process').exec;
+
+    // Function to be executed with a delay
+    function delayedExecution() {
+        console.log("Executing delayed action after 5 seconds");
+
+        // Your logic here
+        exec('node zzzz_add_networkSecurityConfig_manifest.js', (err, stdout, stderr) => {
+            if (err) {
+                console.error("Error executing delayed script:", err);
+                return;
+            }
+            console.log("Zoom plugin script executed successfully.");
+        });
+    }
+
+    // Delay the execution by 5 seconds (5000 ms)
+    setTimeout(delayedExecution, 5000); // Change 5000 to the delay you need in ms
+};
