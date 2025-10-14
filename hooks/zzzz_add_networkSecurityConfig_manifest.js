feaab85b30b792c2225a6002d10c2cb43e1522e0
Jacksun84 <bruno.sol.js@gmail.com>
2025-10-14 10:26:14 +0100

    rm plugins folder

--- hooks/zzzz_add_networkSecurityConfig_manifest.js
+++ hooks/zzzz_add_networkSecurityConfig_manifest.js
@@ -72,26 +72,3 @@ module.exports = function (context) {
         console.warn('  --- ❌ [Zoom Plugin] --- AndroidManifest.xml not found. Make sure the Android platform is added.');
     }
 };
-
-
-
-
-
-console.log("Running hook to install NodeJS requirements");
-
-module.exports = function (context) {
-  var child_process = require('child_process'),
-      deferral = require('q').defer();
-
-  var output = child_process.exec('npm install', {cwd: __dirname}, function (error) {
-    if (error !== null) {
-      console.log('exec error: ' + error);
-      deferral.reject('npm installation failed');
-    }
-    else {
-      deferral.resolve();
-    }
-  });
-
-  return deferral.promise;
-};
